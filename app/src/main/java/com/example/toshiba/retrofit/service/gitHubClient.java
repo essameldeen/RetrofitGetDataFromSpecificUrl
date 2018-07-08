80d86729c30a9e0b05df8e5c870b29776f60b547
esam123 <esammohamed17121996@gmail.com>
2018-07-08 14:21:14 +0200

    dynamic url

--- app/src/main/java/com/example/toshiba/retrofit/service/gitHubClient.java
+++ app/src/main/java/com/example/toshiba/retrofit/service/gitHubClient.java
@@ -5,10 +5,15 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Url;
 
 public interface gitHubClient {
 
     @GET("/users/{user}/repos")
     Call<List<gitHubRepo>> reposForUser(@Path("user") String userName);
+
+    // Connection with dynamic Url
+    @GET
+    Call<List<gitHubRepo>> reposForUserDynamicUrl(@Url String userName);
 
 }
